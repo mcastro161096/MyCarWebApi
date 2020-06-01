2d197decfb77a428c7425d2d4f71940d6a6f95c7
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-06-01 20:09:31 -0300

    Criada a classe BasicAuhtenticationAttribute para ser utilizada nas requisições post, put, delete e get.

---
+++ Filters/BasicAuhtenticationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace MyCarWebApi.Filters
+{
+    public class BasicAuhtenticationAttribute : AuthorizationFilterAttribute
+    {
+        private const string TOKEN = "teste";
+
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            var authorizationHeader =  actionContext.Request.Headers.Authorization;
+            if (authorizationHeader == null || authorizationHeader.Scheme != "Bearer" || authorizationHeader.Parameter != TOKEN)
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
