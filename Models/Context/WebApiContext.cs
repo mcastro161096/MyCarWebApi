ae0ae8cfeae37eb216d4e44f77c7f84f96aab02b
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-06-01 19:47:03 -0300

    Criada a classe context e adicionada a connectionString.

---
+++ Models/Context/WebApiContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCarWebApi.Models
+{
+   public class WebApiContext : DbContext
+    {
+        public WebApiContext() : base("name=DbMyCarWebApi")
+        {
+            Database.SetInitializer<WebApiContext>(new CreateDatabaseIfNotExists<WebApiContext>());
+        }
+        public  DbSet<Pessoa> Pessoas { get; set; }
+
+        public DbSet<ServicoPrestado> ServicosPrestados { get; set; }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+
+
+
+
+    }
+}
