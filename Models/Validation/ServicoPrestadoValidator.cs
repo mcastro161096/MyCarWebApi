e41c20518c1e8f074f7797b8108ebe44cbea51f9
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-06-03 06:13:29 -0300

    Implementadas as classes de mapeaemento e validação para a entidade ServicoPrestado.

--- Models/Validation/ServicoPrestadoValidator.cs
+++ Models/Validation/ServicoPrestadoValidator.cs
@@ -12,19 +12,19 @@ namespace MyCarWebApi.Models
             RuleFor(s => s.ServicoInformado).NotEmpty().WithMessage("O campo  deve ser preenchido.")
                 .Length(1, 50).WithMessage("O serviço informado deve ter no mínimo {minLength} e no máximo {maxLength} caracteres.");
 
-            RuleFor(s => s.DescricaoAdicionalServico).NotEmpty().WithMessage("O campo  deve ser preenchido.");
+            RuleFor(s => s.DescricaoAdicionalServico).NotEmpty().WithMessage("O campo  deve ser preenchido.")
+                .Length(0, 2000).WithMessage("A descrição deve ter no mínimo {minLength} e no máximo {maxLength} caracteres.");
 
-            RuleFor(s => s.).NotEmpty().WithMessage("O campo  deve ser preenchido.");
+            RuleFor(s => s.ValorTotal).NotEmpty().WithMessage("O campo valor total  deve ser preenchido.");
 
-            RuleFor(s => s.).NotEmpty().WithMessage("O campo  deve ser preenchido.");
+            RuleFor(s => s.ValorPago).NotEmpty().WithMessage("O campo valor pago deve ser preenchido.");
 
-            RuleFor(s => s.).NotEmpty().WithMessage("O campo  deve ser preenchido.");
+            RuleFor(s => s.Situacao).NotEmpty().WithMessage("O campo situação deve ser preenchido.")
+                .Length(1, 10).WithMessage("A situação deve ter no mínimo {minLength} e no máximo {maxLength} caracteres.");
 
-            RuleFor(s => s.).NotEmpty().WithMessage("O campo  deve ser preenchido.");
+            RuleFor(s => s.DataServico).NotEmpty().WithMessage("O campo data do serviço deve ser preenchido.");
 
-            RuleFor(s => s.).NotEmpty().WithMessage("O campo  deve ser preenchido.");
 
-            RuleFor(s => s.).NotEmpty().WithMessage("O campo  deve ser preenchido.");
         }
 
     }
